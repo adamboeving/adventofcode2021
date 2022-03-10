8a2cf9bc25647c7d253482d89b9021cde309ec44
adamboeving <adamboeving@yahoo.com>
2022-03-09 20:47:25 -0500

    formatted document

--- day_01/app.js
+++ day_01/app.js
@@ -1,30 +1,13 @@
-
-const fs = require('fs');
-const depths = fs.readFileSync('input.txt').toString().split("\n");
+const fs = require("fs");
+const depths = fs.readFileSync("input.txt").toString().split("\n");
 
 let counter = 0;
 
-// let test = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-
-
-// for (let i = 0; i < test.length; i++) {
-
-//     if(test[i] < test[i + 1] ){
-//         counter++
-//     }
-
-
 for (let i = 0; i < depths.length; i++) {
-
-    if(parseInt(depths[i]) < parseInt(depths[i + 1]) ){
-        counter++
-    }
-
-    // if(depths[i < depths [i + 1]]){
-    //     counter++
-    // }
-	
+  if (parseInt(depths[i]) < parseInt(depths[i + 1])) {
+    counter++;
+  }
 }
 
-console.log(counter)
-console.log(depths.length)
+console.log(counter);
+console.log(depths.length);
